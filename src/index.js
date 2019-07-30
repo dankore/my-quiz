113e785b1462e89b22f0d30a02721d7b8ccdefce
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2019-07-30 03:10:38 -0500

    Styling

--- src/index.js
+++ src/index.js
@@ -13,16 +13,17 @@ const startButton = document.querySelector(".start-button");
 
 let eachBundle, currentIndex;
 
-const start = () => {
+const startButtonFunction = () => {
   eachBundle = quizBundle.sort();
   currentIndex = 0;
   quizContainer.classList.remove("hide");
   startButton.classList.add("hide");
   nextQuestion();
 };
-startButton.addEventListener("click", start);
+startButton.addEventListener("click", startButtonFunction);
 
 const nextQuestion = () => {
+  showRightOrWrongContainer.classList.add("hide");
   reset();
   if (currentIndex < quizBundle.length) {
     show(eachBundle[currentIndex]);
@@ -31,19 +32,40 @@ const nextQuestion = () => {
     quizContainer.classList.add("hide");
     startButton.classList.remove("hide");
   }
-  console.log(currentIndex, quizBundle.length);
+  // Show progress
+  currentQuestionNumber.innerText = currentIndex;
+  totalQuestions.textContent = quizBundle.length;
 };
 nextQuestionButton.addEventListener("click", nextQuestion);
 
-const show = bundle => {
+const show = () => {
   questionsHolder.innerText = eachBundle[currentIndex].question;
 
   eachBundle[currentIndex].options.forEach(option => {
-    const button = document.createElement("button");
-    button.innerText = option.text;
+    const button = document.createElement("input");
+    button.type = "checkbox";
+    button.id = "options";
+    const label = document.createElement("label");
+    label.htmlFor = "options";
+    label.appendChild(document.createTextNode(option.text));
+    label.classList.add("label");
+
     button.classList.add("btn");
     optionsHolder.appendChild(button);
-    button.addEventListener("click", selectAnswer);
+    optionsHolder.appendChild(label);
+    console.log(option.correct);
+    button.addEventListener("click", () => {
+      const answer = option.correct;
+      if (answer === true) {
+        showRightOrWrongContainer.classList.remove("hide");
+        showRightOrWrongContainer.innerText = "Correct";
+        showRightOrWrongContainer.style.color = "green";
+      } else {
+        showRightOrWrongContainer.classList.remove("hide");
+        showRightOrWrongContainer.style.color = "red";
+        showRightOrWrongContainer.innerText = "Wrong";
+      }
+    });
   });
 };
 const reset = () => {
@@ -52,31 +74,56 @@ const reset = () => {
   }
 };
 
-const selectAnswer = e => {
-  const selectedButton = e.target;
-  const answer = selectedButton.corret;
-};
+// const show = bundle => {
+//   questionsHolder.innerText = eachBundle[currentIndex].question;
+
+//   eachBundle[currentIndex].options.forEach(option => {
+//     const button = document.createElement("button");
+//     button.innerText = option.text;
+//     button.classList.add("btn");
+//     optionsHolder.appendChild(button);
+//     console.log(option.correct);
+//     button.addEventListener("click", () => {
+//       const answer = option.correct;
+//       if (answer === true) {
+//         showRightOrWrongContainer.classList.remove("hide");
+//         showRightOrWrongContainer.innerText = "Correct";
+//         showRightOrWrongContainer.style.color = "green";
+//       } else {
+//         showRightOrWrongContainer.classList.remove("hide");
+//         showRightOrWrongContainer.style.color = "red";
+//         showRightOrWrongContainer.innerText = "Wrong";
+//       }
+//     });
+//   });
+// };
+// const reset = () => {
+//   while (optionsHolder.firstChild) {
+//     optionsHolder.removeChild(optionsHolder.firstChild);
+//   }
+// };
+
 const quizBundle = [
   {
     question: ["What is a life"],
     options: [{ text: "a", correct: true }, { text: "b", correct: false }]
   },
-  // {
-  //   question: ["What is water?"],
-  //   options: [{ text: "c", correct: true }, { text: "d", correct: false }]
-  // }
+  {
+    question: ["What is water?"],
+    options: [{ text: "c", correct: true }, { text: "d", correct: false }]
+  },
   {
     question: ["What is a me?"],
     options: [
       { text: "apple", correct: true },
       { text: "ornage", correct: false }
     ]
+  },
+  {
+    question: ["What is er?"],
+    options: [
+      { text: "cat", correct: true },
+      { text: "elephant", correct: false }
+    ]
   }
-  // {
-  //   question: ["What is er?"],
-  //   options: [
-  //     { text: "cat", correct: true },
-  //     { text: "elephant", correct: false }
-  //   ]
-  // }
 ];
